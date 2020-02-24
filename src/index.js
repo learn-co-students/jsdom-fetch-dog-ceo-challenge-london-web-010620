f9e002bd4f878b1aa255b40df69da2aca309291e
simon <sjtucker28@gmail.com>
2020-02-24 09:30:06 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const dogImgContrainer = document.getElementById('dog-image-container')
+console.log(dogImgContrainer)
+
+const getURl = fetch(imgUrl)
+    .then(response => response.json()
+        .then(result => {
+            result.message.forEach(imageUrl => addImageFromUrl(imageUrl))
+        }))
+
+const addImageFromUrl = (url) => {
+    const imageTag = document.createElement('img')
+    imageTag.src = url
+    dogImgContrainer.appendChild(imageTag)
+}
